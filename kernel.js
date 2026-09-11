5fc8e31ffd88b2ef0be763c2a42c6ab87023824a
iki1uc <entertrainer@web.de>
2026-09-11 17:29:59 +0200

    Update kernel.js

--- kernel.js
+++ kernel.js
@@ -1,1 +1,23 @@
+MASTER_KERNEL = {
+    init:  KIT.init,
+    build: ULTRA_KERNEL.build,
+    send:  ULTRA_KERNEL.sendToALL,
+    pipe:  KERNEL.runRithPipeline,
+    orbit: ZaM.orbit,
+    ui:    UI.init,
+    eco:   ECONOMY.init,
+    work:  WORK.start,
+    groub: GROUB.start,
+    start(){
+        KIT.init();
+        const matrix = ULTRA_KERNEL.build();
+        ULTRA_KERNEL.sendToALL(matrix);
+        WORK.start();
+        GROUB.start();
+        UI.init();
+        ECONOMY.init();
+        ZaM.orbit();
+        return matrix;
+    }
+}
 
